4c516b702ddebc445dbf454519c88da941881285
Hrisahbh-G <hrishabhgupta96@gmail.com>
2023-12-11 12:56:06 +0530

    Add session login in app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+  # get 'sessions/new'
+  # get 'sessions/create'
+  # get 'sessions/destroy'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  # delete '/logout', to: 'sessions#destroy'
+  delete '/logout', to: 'sessions#destroy', as: :logout
+
+  get '/home', to: 'users#index', as: :home
+  # root 'users#index'
+  # get '/social-pulse.com', to: 'application#greeting_page'
+  root 'application#greeting_page'
+
+  resources :users, only: [:new, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
